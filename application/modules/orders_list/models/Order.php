52bf275cdf9f723dbdfe6dfb61e8b3daf0257eb9
Сергей Марченко <sergejmarcenko@C02VM1QAJ1G5.local>
2024-03-14 14:35:43 +0300

    Add type to Order class contants

--- application/modules/orders_list/models/Order.php
+++ application/modules/orders_list/models/Order.php
@@ -6,13 +6,13 @@ use yii\db\ActiveRecord;
 
 class Order extends ActiveRecord
 {
-    public const STATUS_CODE_PENDING = 0;
-    public const STATUS_CODE_IN_PROGRESS = 1;
-    public const STATUS_CODE_COMPLETED = 2;
-    public const STATUS_CODE_CANCELED = 3;
-    public const STATUS_CODE_ERROR = 4;
-    public const MODE_CODE_MANUAL = 0;
-    public const MODE_CODE_AUTO = 1;
+    public const int STATUS_CODE_PENDING = 0;
+    public const int STATUS_CODE_IN_PROGRESS = 1;
+    public const int STATUS_CODE_COMPLETED = 2;
+    public const int STATUS_CODE_CANCELED = 3;
+    public const int STATUS_CODE_ERROR = 4;
+    public const int MODE_CODE_MANUAL = 0;
+    public const int MODE_CODE_AUTO = 1;
 
     public static function tableName()
     {
